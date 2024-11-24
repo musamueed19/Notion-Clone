c3ff2d4e362bf16438a267a482c9eb4e25b328f0
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-11-24 21:16:24 +0500

    setup part-1

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col w-fit">
+      Lets build an AI productivity APP
+      
+      <Button>Click Me</Button>
+    </div>
+  );
+}
